dddc017f61447dd5fa5ce0efea449259f5add8d4
Medha0909 <103240973+Medha0909@users.noreply.github.com>
2025-07-03 23:45:55 +0530

    Update login.js

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -10,7 +10,7 @@ function Login() {
     e.preventDefault();
     localStorage.setItem("uniqueId",credentials.password)
     window.sessionStorage.setItem("email", credentials.email);
-    const response = await fetch("http://localhost:8080/reg/login", {
+    const response = await fetch("https://eventure-nozw.onrender.com/reg/login", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
